54245cf3f673914ade4f16d6a7ab15f9451691cd
Stu Ratcliffe <email@sturatcliffe.co.uk>
2019-06-27 18:59:35 +0100

    Initial

--- MartenProjectViewIssue/Program.cs
+++ MartenProjectViewIssue/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Linq;
+using Baseline.Dates;
+using FluentAssertions;
+using Marten;
+using Marten.Events.Projections.Async;
 
 namespace MartenProjectViewIssue
 {
@@ -6,7 +11,68 @@ namespace MartenProjectViewIssue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var store = DocumentStore.For(_ =>
+            {
+                _.Connection("Server=127.0.0.1;Port=5432;Database=marten;User Id=postgres;Password=postgres;");
+                _.AutoCreateSchemaObjects = AutoCreate.All;
+                _.Events.InlineProjections.AggregateStreamsWith<User>();
+                _.Events.ProjectView<User, Guid>().DeleteEvent<UserDeleted>();
+            });
+
+            var userId = Guid.NewGuid();
+
+            using (var session = store.OpenSession())
+            {
+                session.Events.Append(userId, new UserCreated {Id = userId, Name = "Stu"});
+                session.SaveChanges();
+
+                session.Query<User>().Count().Should().Be(1);
+
+                session.Events.Append(userId, new UserDeleted {Id = userId});
+                session.SaveChanges();
+
+                session.Query<User>().Count().Should().Be(0);
+
+                var settings = new DaemonSettings
+                {
+                    LeadingEdgeBuffer = 0.Seconds()
+                };
+
+                using (var daemon = store.BuildProjectionDaemon(new[] {typeof(User)}, settings: settings))
+                {
+                    daemon.RebuildAll().ConfigureAwait(false).GetAwaiter().GetResult();
+                }
+
+                //fails as user is back in the database after rebuilding
+                session.Query<User>().Count().Should().Be(0);
+            }
+        }
+
+        public class User
+        {
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+
+            public void Apply(UserCreated e)
+            {
+                Id = e.Id;
+                Name = e.Name;
+            }
+
+            public void Apply(UserDeleted e)
+            {
+            }
+        }
+
+        public class UserCreated
+        {
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class UserDeleted
+        {
+            public Guid Id { get; set; }
         }
     }
 }
